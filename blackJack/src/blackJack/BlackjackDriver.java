ea3c5f81e2608a7c8c28db49b67b7560d4b0ebe7
unknown <pravatktimsina@gmail.com>
2024-03-26 22:19:03 -0700

    Initializing blackjack codebase

---
+++ blackJack/src/blackJack/BlackjackDriver.java
@@ -0,0 +1,14 @@
+package blackJack;
+
+/**
+ * @author Pravat
+ *
+ */
+public class BlackjackDriver {
+
+	public static void main(String[] args) {
+		System.out.println("initializing Blackjack Project ");
+
+	}
+
+}
