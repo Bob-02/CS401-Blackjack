c43c8df1593440689a9b82749c627cf6d561a56a
Bob-02 <111538673+Bob-02@users.noreply.github.com>
2024-04-18 10:13:50 -0700

    Code cleanup
    Removed classes within the Server class and created two separate classes. A ClientHandler class to handle the clients connected to the server and a Date class to print dates and times.

---
+++ blackJack/src/ClientHandler.java
@@ -0,0 +1,101 @@
+//ClientHandler class
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+	private final Socket clientSocket;
+	private static int count = 1;
+    private final int id;
+
+	// Constructor
+	public ClientHandler(Socket socket)
+	{
+		this.clientSocket = socket;
+		this.id = count++;
+	}
+
+	public void run()
+	{
+		try {
+				
+		    // Data TOO the client
+	        OutputStream outputStream = clientSocket.getOutputStream();
+	        ObjectOutputStream objectOutputStream =
+	        		new ObjectOutputStream(outputStream);
+	        
+	        // Data FROM the client
+	        InputStream inputStream = clientSocket.getInputStream();
+	        ObjectInputStream objectInputStream = 
+	        		new ObjectInputStream(inputStream);
+	        
+	        // get message from client.
+	        Message current = (Message) objectInputStream.readObject();
+	        
+	        // If we get a NEW login message, check the text supplied in the
+	        // message and check the server account details in text file.
+	        
+	        // A valid dealer, type login, status new, text, dealer.
+	        // A valid player, type login, status new, text, player.
+	        if(current.getType() == Type.Login 
+	        		&& current.getStatus() == Status.New) {
+	        	
+	        	
+		        // look in the server here!!!
+		        // Boolean valid = loginUser(current.getText());
+		        // if(valid) {
+	
+	        	// IF account details found Set status to success
+	        	current.setStatus(Status.Success);
+	        	
+	        	// Send updated message back to the client
+	        	objectOutputStream.writeObject(current);
+	        	System.out.println("Login Successful from Client #"
+	        						+ id + "!\n");
+	        	
+	        	//}
+	        	
+	        	// Keep reading for text messages until we get a logout message.'
+	        	// add 'valid' flag to loop.
+	        	while(current.getType() != Type.Logout
+	        			&& current.getStatus() == Status.New) {
+	        		
+	        		// Get a message from the user. 
+	        		current = (Message) objectInputStream.readObject();
+	        		
+	        		//
+	        		// Switch to handle all the various types of messages?
+	        		//
+	        		
+	        		}
+	        	
+	        	
+	        	// On receipt of logout, a ‘logout message’ will be returned
+	        	// with status of ‘success’, then the connection will be
+	        	// closed by the server and the thread terminates.
+	        	current.setStatus(Status.Success);
+	        	
+	        	// Send updated message back to the client
+	        	objectOutputStream.writeObject(current);
+	        	System.out.println("Client #" + id + " Logged out at "
+	        			+ new Date().getCurrentDate());
+	        }
+
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		} 
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+}
---
+++ blackJack/src/Date.java
@@ -0,0 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Date {
+	private String date;
+	
+	public Date() {
+		date = getCurrentDate();
+	}
+	
+	public String getDate() {
+		return this.date;
+	}
+	
+	public String getCurrentDate() {
+		DateTimeFormatter dateFormat =
+				DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		return now.format(dateFormat);
+	}
+}
