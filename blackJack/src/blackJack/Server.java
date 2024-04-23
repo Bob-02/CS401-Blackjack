307d3dd8baf492b7d7d43835291488db5b2461f5
Bob-02 <111538673+Bob-02@users.noreply.github.com>
2024-04-23 12:31:27 -0700

    Added Setters and getters

--- blackJack/src/blackJack/Server.java
+++ blackJack/src/blackJack/Server.java
@@ -3,14 +3,16 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 
 
 public class Server {
     public static void main(String[] args) 
     		throws IOException, ClassNotFoundException {
     	
-    	ServerDetails serverDetails = new ServerDetails();
-    	
+    	// Load valid registered Players and Dealers from both files.
+    	loadValidUsers("players.txt", "dealers.txt");
+
     	// Print local host to console. Let others know where to connect.
         InetAddress localHost = InetAddress.getLocalHost();
         System.out.println(localHost.getHostAddress());
@@ -41,11 +43,12 @@ public class Server {
 
 				// create a new thread object
 				ClientHandler clientSock = 
-						new ClientHandler(client, serverDetails);
+						new ClientHandler(client);
 
 				// This thread will handle the client
 				// separately
 				new Thread(clientSock).start();
+				
 			}
 		}
 		catch (IOException e) {
@@ -62,4 +65,55 @@ public class Server {
 			}
 		}
     }
+    
+	private static String serverName;			
+	private static List<Game> games;			
+	private static List<Player> validPlayers;	
+	private static List<Dealer> validDealers;	
+	private static List<Player> onlinePlayers;	
+	private static List<Dealer> onlineDealers;	
+	private static double casinoFunds;	
+    
+	public static double getCasinoFunds() {
+		return casinoFunds;
+	}
+
+	public static void setCasinoFunds(double casinoFunds) {
+		Server.casinoFunds = casinoFunds;
+	}
+
+	public static List<Game> getGames() {
+		return games;
+	}
+
+	public static List<Player> getValidPlayers() {
+		return validPlayers;
+	}
+
+	public static List<Dealer> getValidDealers() {
+		return validDealers;
+	}
+
+	public static List<Player> getOnlinePlayers() {
+		return onlinePlayers;
+	}
+
+	public static List<Dealer> getOnlineDealers() {
+		return onlineDealers;
+	}
+
+	
+	public static String getServerName() {
+		return serverName;
+	}
+	
+	
+	// With the supplied filename for Players and Dealers. Registered users will
+	// be added to each valid Dealer and Player list.
+	public static void loadValidUsers(String dealerFilename, 
+									  String playerFilename) {
+		
+	}
+    
 }
+
