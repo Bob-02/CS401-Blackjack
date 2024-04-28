b6543885601f589a80107b6949dc67cca1f8f68a
Rebecca <rdthsk@gamil.com>
2024-04-28 12:13:34 -0700

    JUnit PlayingDeck test class
    Also, test classes for Deck, Dealer, and Player are created, test cases not implemented yet

--- blackJack/src/AllTests.java
+++ blackJack/src/AllTests.java
@@ -3,7 +3,8 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ CardTest.class, TableTest.class })
+@SelectClasses({ CardTest.class, TableTest.class, PlayingDeckTest.class, DeckTest.class, DealerTest.class,
+		PlayerTest.class })
 class AllTests {
 
 }
---
+++ blackJack/src/DeckTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+
+	@Test
+	void testDeck() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCards() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGenerateDeck() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
